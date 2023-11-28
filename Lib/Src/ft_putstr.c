d85b99d5417f54d0d98fe71f529a233c5d8fe7b7
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-11-28 17:30:19 +0100

    BONNE SA MERE

---
+++ Lib/Src/ft_putstr.c
@@ -0,0 +1,23 @@
+#include "../includes/ft_printf.h"
+#include <unistd.h>
+
+int	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+	{
+		i = write(1, "(null)", 6);
+		return (i);
+	}
+	else
+	{
+		while (str[i] != '\0')
+		{
+			ft_putchar(str[i]);
+			i++;
+		}
+	}
+	return (i);
+}
---
+++ Src/init_env/init_env.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_env.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acatusse <acatusse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/20 21:38:04 by fililafrapp       #+#    #+#             */
+/*   Updated: 2023/11/25 12:18:39 by acatusse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// env est determiné par le 3e argument du main "char **env", qui permet 
+// d'accéder aux variables globales définies par le système d'exploitation.
+
+#include "../../Include/minishell.h"
+
+/*
+	Utilisé dans la fonction if_no_env, il crée un nouveau noeud pour
+	une variable OLDPWD.
+*/
+void	no_env_node_init(char *str, t_env **env)
+{
+	t_env	*new_node;
+
+	new_node = (t_env *)malloc(sizeof(t_env));
+	if (new_node == NULL)
+	{
+		perror("Erreur lors de l'allocation de mémoire");
+		return ;
+	}
+	new_node->content = ft_strdup(str);
+	new_node->next = NULL;
+	*env = new_node;
+}
+
+/*
+	Est utilisée dans le cas où aucune variable d'environnement n'est passée
+	à Minishell. La fonction getwcd est utilisée pour trouver le répertoire
+	de travail courant (PWD). Ce path sera mit dans une variable temporaire
+	qui sera elle même mise dans un noeud renvoyé à init_env_list.
+	Sans variables d'environnement, certaines fonctionnalités de base du 
+	shell pourraient ne pas fonctionner correctement.
+*/
+t_env	*if_no_env(t_env *final)
+{
+	char	*tmp;
+
+	tmp = getcwd(NULL, 0);
+	tmp = ft_strjoin_2("OLDPWD=", tmp);
+	no_env_node_init(tmp, &final);
+	return (final);
+}
+
+/*
+	La liste chaînée d'environnement est stockée dans Utils.
+	Elle initialise env_lst et head_env_lst avec la liste
+	qu'on a créé puis initialise le code d'erreur, le heredoc
+	et ?ret? à 0.
+*/
+t_exec	*init_env(char **env)
+{
+	t_exec	*utils;
+
+	utils = ft_malloc(sizeof(t_exec));
+	if (!utils)
+		return (NULL);
+	utils->env_lst = NULL;
+	if (env)
+		utils->env_lst = init_env_list(env);
+	utils->head_env_lst = utils->env_lst;
+	utils->err = 0;
+	utils->ret = 0;
+	utils->is_here_doc = 0;
+	return (utils);
+}

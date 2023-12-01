b969ab666edbf38dbe709f8c7692ea2d92ac77dd
Lukas GOURE <lgoure@made-f0Ar4s5.clusters.42paris.fr>
2023-12-01 13:01:31 +0100

    commentaire

--- objs/built_in/ft_exit.d
+++
@@ -1,5 +0,0 @@
-objs/built_in/ft_exit.o: srcs/built_in/ft_exit.c \
-  srcs/built_in/../../include/minishell.h \
-  srcs/built_in/../../include/../Lib/includes/lib.h \
-  srcs/built_in/../../include/../Lib/includes/ft_printf.h \
-  srcs/built_in/../../include/../Lib/includes/get_next_line.h
--- objs/exec/redir_child.d
+++
@@ -1,5 +0,0 @@
-objs/exec/redir_child.o: srcs/exec/redir_child.c \
-  srcs/exec/../../include/minishell.h \
-  srcs/exec/../../include/../Lib/includes/lib.h \
-  srcs/exec/../../include/../Lib/includes/ft_printf.h \
-  srcs/exec/../../include/../Lib/includes/get_next_line.h
